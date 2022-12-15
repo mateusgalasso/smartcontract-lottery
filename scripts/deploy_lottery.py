8ad8ffb5879ab83f32d9d4281e6cdcab47f4f1b5
Mateus Galasso <mateusgalasso@fbb.org.br>
2022-12-15 14:59:13 -0300

    first commit

---
+++ scripts/deploy_lottery.py
@@ -0,0 +1,53 @@
+import time
+
+from scripts.helpful_scripts import get_account, get_contract, fund_with_link
+from brownie import Lottery, config, network
+
+
+def deploy_lottery():
+    account = get_account()
+    lottery = Lottery.deploy(
+        get_contract("eth_usd_price_feed").address,
+        get_contract("vrf_coordinator").address,
+        get_contract("link_token").address,
+        config["networks"][network.show_active()]["fee"],
+        config["networks"][network.show_active()]["keyhash"],
+        {"from": account},
+        publish_source=config["networks"][network.show_active()].get("verify", False)
+    )
+    print("Deployed Lottery!!")
+    return lottery
+
+
+def start_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    starting_txn = lottery.startLottery({"from": account})
+    starting_txn.wait(1)
+    print("The lottery is started")
+
+
+def enter_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    value = lottery.getEntranceFee() + 100000000
+    tx = lottery.enter({"from": account, "value": value})
+    tx.wait(1)
+    print("You entered in the lottery")
+
+
+def end_lottery():
+    account = get_account()
+    lottery = Lottery[-1]
+    fund_with_link(lottery.address)
+    ending_transaction = lottery.endLottery({"from": account})
+    ending_transaction.wait(1)
+    time.sleep(60)
+    print(f"And the winner is {ending_transaction.recentWinner()}")
+
+
+def main():
+    deploy_lottery()
+    start_lottery()
+    enter_lottery()
+    end_lottery()
---
+++ tests/test_lottery_unit.py
@@ -0,0 +1,72 @@
+from brownie import Lottery, accounts, config, network, exceptions, Lottery
+from scripts.deploy_lottery import deploy_lottery
+from scripts.helpful_scripts import LOCAL_BLOCKCHAIN_ENVIRONMENTS, get_account, fund_with_link, get_contract
+from web3 import Web3
+import pytest
+
+
+def test_get_entrace_fee():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    expected_entrance_fee = Web3.toWei(0.25, "ether")
+    entrance_fee = lottery.getEntranceFee()
+
+    assert expected_entrance_fee == entrance_fee
+
+
+def test_cant_enter_unless_started():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.enter({"from": get_account(), "value": lottery.getEntranceFee()})
+
+
+def test_can_start_and_enter_lottery():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    account = get_account()
+    lottery = deploy_lottery()
+    lottery.startLottery({"from": account})
+    # Asserrt lottery  is open
+    assert lottery.lottery_state() == 0
+    # Enter lottery
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    assert lottery.players(0) == account
+
+
+def test_can_end_lottery():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    with pytest.raises(exceptions.VirtualMachineError):
+        lottery.endLottery()
+
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    lottery.endLottery({"from": account})
+    assert lottery.lottery_state() == 2
+
+
+def test_can_pick_winner_correctly():
+    if network.show_active() not in LOCAL_BLOCKCHAIN_ENVIRONMENTS:
+        pytest.skip()
+    lottery = deploy_lottery()
+    account = get_account()
+    lottery.startLottery({"from": account})
+    lottery.enter({"from": account, "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=1), "value": lottery.getEntranceFee()})
+    lottery.enter({"from": get_account(index=2), "value": lottery.getEntranceFee()})
+    fund_with_link(lottery)
+    transaction = lottery.endLottery({"from": account})
+    request_id = transaction.events["RequestedRandomness"]["requestId"]
+    get_contract('vrf_coordinator').callBackWithRandomness(request_id, 777, lottery.address, {"from": account})
+    starting_balance_of_account = account.balance()
+    balance_of_lottery = lottery.balance()
+    # Assert
+    assert lottery.recentWinner() == account
+    assert lottery.balance() == 0
+    assert account.balance() == starting_balance_of_account + balance_of_lottery
